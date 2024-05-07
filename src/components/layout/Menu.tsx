4bf5d9a8a7bf12a496d95cc024889a7a928aadce
devzeld <zeld.exe.dev@gmail.com>
2024-05-07 22:34:12 +0200

    menu effects

--- src/components/layout/Menu.tsx
+++ src/components/layout/Menu.tsx
@@ -1,16 +1,32 @@
 import '../../styles/components/_menu.scss';
-import {useNavigate} from "react-router-dom";
+import {useNavigate, useLocation} from "react-router-dom";
 
 function Menu() {
     const navigate = useNavigate();
+
+    const menuLinkEls = document.querySelectorAll('.menu_link');
+    
+    let location = useLocation().pathname === '/' ? 'Home' : useLocation().pathname.substring(1, 2).toUpperCase() + useLocation().pathname.substring(2);
+    menuLinkEls.forEach(menuLinkEl => {
+        if(location === menuLinkEl.textContent){
+            document.querySelector('.active')?.classList.remove('active');
+            menuLinkEl.classList.add('active');
+            console.log("ysp")
+        }
+    })
+
     return (
         <>
             <nav className="menu">
                 <ul className="menu-list">
-                    <li><a onClick={() => navigate("/home")}>Home</a></li>
-                    <li><a onClick={() => navigate("/projects")}>Projects</a></li>
-                    <li><a onClick={() => navigate("/about")}>About</a></li>
-                    <li><a onClick={() => navigate("/contacts")}>Contacts</a></li>
+                    <li className={'menu_item'}>
+                        <a className = {"menu_link"} onClick={() => {navigate("/home");}}>Home</a></li>
+                    <li className={'menu_item'}>
+                        <a className = {"menu_link"} onClick={() => {navigate("/projects");}}>Projects</a></li>
+                    <li className={'menu_item'}>
+                        <a className = {"menu_link"} onClick={() => {navigate("/about");}}>About</a></li>
+                    <li className={'menu_item'}>
+                        <a className = {"menu_link"} onClick={() => {navigate("/contacts");}}>Contacts</a></li>
                 </ul>
             </nav>
         </>
