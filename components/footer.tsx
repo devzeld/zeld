48741cd083ca6d9225ee77b64e181defd8f9f060
devzeld <zeld.exe.dev@gmail.com>
2025-02-13 22:40:28 +0100

    Aggiunto il componente Footer con icone social e aggiornato lo stile; aggiunta dipendenza react-icons

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React from "react";
+import Link from "next/link";
+
+import { PiInstagramLogoDuotone } from "react-icons/pi";
+import { VscGithub } from "react-icons/vsc";
 
 export default function Footer() {
-    return (
-        <footer>
-            Footer
-        </footer>
-    );
-};
+  return (
+    <footer>
+      <div className="social">
+        <p>Here: </p>
+        <ul>
+          <li>
+            <Link href={""}>
+              <PiInstagramLogoDuotone size={"30px"} />
+            </Link>
+          </li>
+          <li>
+            <Link href={""}>
+              <VscGithub size={"30px"} />
+            </Link>
+          </li>
+          <li>
+            <Link href={""}></Link>
+          </li>
+        </ul>
+      </div>
+      <p className="rights">&copy; {new Date().getFullYear()} Me. All rights reserved.</p>
+    </footer>
+  );
+}
