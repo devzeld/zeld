e10858fb3bce64db06cc221919429d3b30414601
eldis.izeirovski <zeld.exe.dev@gmail.com>
2025-02-14 13:50:41 +0100

    Fetch real-time weather data from Tomorrow.io API in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
 export default function Home() {
+
+  const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+
+  fetch('https://api.tomorrow.io/v4/weather/realtime?location=mestre%20venice&apikey=vaW9YSGpj0vCyVlpGtzNAowQh458gjzi', options)
+  .then(response => response.json())
+  .then(response => console.log(response.data))
+  .catch(err => console.error(err));
+
+
   return (
     <section>
+
     </section>
   );
 }
