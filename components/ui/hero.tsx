bd44bfb9f076883b0b518bb7d8d0d952350fb40c
devzeld <zeld.exe.dev@gmail.com>
2025-02-22 00:23:08 +0100

    Refactor Hero component by removing unused motion animations; update CSS transition timing function for smoother effects and remove unused Spotify SDK dependency

--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -1,16 +1,6 @@
 import React from "react";
-import { motion, HTMLMotionProps } from "framer-motion";
+import { motion } from "framer-motion";
 
-interface CustomMotionDivProps extends HTMLMotionProps<"div"> {
-    initial: "initial" | "final";
-  }
-
-export default function Hero ({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="hero">
-            <motion.div animate={{ scale: 2, transition: { duration: 2 } }}>
-                {children}
-            </motion.div>
-        </div>
-    );
+export default function Hero({ children }: { children: React.ReactNode }) {
+  return <div className="hero">{children}</div>;
 }
