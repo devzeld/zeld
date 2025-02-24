9336352dab531503a8b8aadc837f6c2bf6702653
eldis.izeirovski <zeld.exe.dev@gmail.com>
2025-02-24 12:06:11 +0100

    Update global CSS font weight and refine header component styles for improved navigation

--- components/header.tsx
+++ components/header.tsx
@@ -1,27 +1,35 @@
+"use client";
+
 import React from 'react';
 import Link from 'next/link';
+import { usePathname} from 'next/navigation';
+
 
 export default function Header() {
+
+    const path = usePathname();
+    const isActive = (pathname: string) => path === pathname;
+
 
     const styles = {
         li: 'underline transition delay-150 duration-200 all',
     }
 
     return (
-        <header className='flex justify-center items-center py-4 px-8 bg-gradient-to-r from-blue-500 to-blue-700 text-white'>
+        /*header = bg-gradient-to-r from-blue-500 to-blue-700*/
+        <header className='flex justify-center items-center py-4 px-8 text-white'>
             <nav className='flex text-align-center'>
-                <ul className='flex space-x-4 content-center font-semibold text-lg'>
-            
-                    <li className={styles.li}>
+                <ul className='flex space-x-8 content-center font-semibold text-xl'>
+                    <li className={styles.li + isActive('/') ? 'text-red-500' : ''}>
                         <Link href='/'>Home</Link>
                     </li>
-                    <li className={styles.li}>
+                    <li className={styles.li + isActive('/about') ? 'text-red-500' : ''}>
                         <Link href='/about'>About</Link>
                     </li>
-                    <li className={styles.li}>
+                    <li className={styles.li + isActive('/projects') ? 'text-red-500' : ''}>
                         <Link href='/projects'>Projects</Link>
                     </li>
-                    <li className={styles.li}>
+                    <li className={styles.li + isActive('/contact') ? 'text-red-500' : ''}>
                         <Link href='/contact'>Contact</Link>
                     </li>
                 </ul>
